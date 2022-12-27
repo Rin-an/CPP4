b59de11b611b4bc14f9471ce1543771f9e294ced
Sahar Sadiki <ssadiki@e2r13p2.1337.ma>
2022-12-27 02:21:01 +0100

    pushing day04

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ssadiki <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 18:58:57 by ssadiki           #+#    #+#             */
-/*   Updated: 2022/12/27 02:05:34 by ssadiki          ###   ########.fr       */
+/*   Updated: 2022/12/27 02:20:17 by ssadiki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,30 @@
 
 int main()
 {
-	//AAnimal aa; won't be instanciated 
-	const Dog* 		j = new Dog();
-	const Cat* 		i = new Cat();
+	{
+		const std::string	*tab;
+		Cat	org;
+		org.getBrain()->setTab("Drink water");
+		{
+			Cat cpy = org;
+			cpy.getBrain()->setTab("Drink choco");
+		}
+		tab = org.getBrain()->getTab();
+		for(int i = 0; i < 3; i++)
+			std::cout << tab[i] << std::endl;
+	}
+	{
+		//AAnimal aa; won't be instanciated
+		const Dog*       j = new Dog();
+		const Cat*       i = new Cat();
 
-	std::cout << "********Right behavior**********" << std::endl;
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); 
-	j->makeSound();
-	delete(j);
-	delete(i);
+		std::cout << j->getType() << " " << std::endl;
+		std::cout << i->getType() << " " << std::endl;
+		i->makeSound();
+		j->makeSound();
+		delete(j);
+		delete(i);
+
+	}
 	return 0;
 }
